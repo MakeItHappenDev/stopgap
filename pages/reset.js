edfeb11161fe926930a3de37c3ce8865102c12bc
PookMook <arthur.juchereau@gmail.com>
2019-12-30 22:12:25 -0500

    add reset pouch DBs page

---
+++ pages/reset.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import PouchDB from 'pouchdb'
+
+const formDB = new PouchDB('form',{auto_compaction: true})
+const imageDB = new PouchDB('photos',{auto_compaction: true})
+const saveDB = new PouchDB('savedForms',{auto_compaction: true})
+
+export default () => {
+  formDB.destroy();
+  imageDB.destroy();
+  saveDB.destroy();
+  return(
+    <p>Reseting pouchDB(s)</p>
+  )
+}
