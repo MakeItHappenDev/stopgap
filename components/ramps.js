7077d654ba3dad824128e19a483dc2a73cdf398a
PookMook <arthur.juchereau@gmail.com>
2020-01-05 15:36:09 -0500

    grid for ramp

--- components/ramps.js
+++ components/ramps.js
@@ -53,11 +53,8 @@ const DisplayRamp = (props) => {
 
   return(
     <article className={styles.ramp}>
-      <p>Business name : {props.businessName}</p>
-      <p>business address: {props.businessAddress}</p>
-      {props.imagesURL && props.imagesURL.map(photo=>(
-        <figure><img src={photo} /></figure>
-      ))}
+      <h1>Business name : {props.businessName}</h1>
+      <h2>business address: {props.businessAddress}</h2>
       <p className={props.singleStep === "Yes" ? styles.correct:styles.false}>Single Step</p>
       <p className={props.nonResidential === "Yes" ? styles.correct:styles.false}>Non Residential</p>
       <p className={props.Owner === "Yes" ? styles.correct:styles.false}>Owner</p>
@@ -65,6 +62,9 @@ const DisplayRamp = (props) => {
       <p className={props.wideStep === "Yes" ? styles.correct:styles.false}>Wide step</p>
       <p className={props.preperHeightStep === "Yes" ? styles.correct:styles.false}>Proper height</p>
       <p className={props.contactMe === "Yes" ? styles.correct:styles.false}>Contact me : {props.email}</p>
+      {props.imagesURL && props.imagesURL.map(photo=>(
+        <figure><img src={photo} /></figure>
+      ))}
     </article>
   )
 }
